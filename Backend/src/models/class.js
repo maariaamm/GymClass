deebffc667e916c367091eb712587407dbbe7e05
maariaamm <mariam_97@live.se>
2025-11-14 10:45:53 +0100

    fix: change date field type to Date and enhance toJSON method for Swedish time zone conversion

--- Backend/src/models/class.js
+++ Backend/src/models/class.js
@@ -1,16 +1,36 @@
 import mongoose from "mongoose";
 
+
 const classSchema = new mongoose.Schema({
   title: String,
   description: String,
   category: String,
   trainer: String,
   trainerId: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
-  date: String,
+  date: { type: Date },
   time: String,
   maxParticipants: Number,
   imageUrl: String,
   participants: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
 }, { timestamps: true });
+
+classSchema.methods.toJSON = function () {
+  const obj = this.toObject();
+
+  // converts to swedish time zone
+  if (obj.createdAt) {
+    obj.createdAt = new Date(obj.createdAt).toLocaleString("sv-SE", {
+      timeZone: "Europe/Stockholm",
+    });
+  }
+
+  if (obj.updatedAt) {
+    obj.updatedAt = new Date(obj.updatedAt).toLocaleString("sv-SE", {
+      timeZone: "Europe/Stockholm",
+    });
+  }
+
+  return obj;
+};
 
 export default mongoose.model("Class", classSchema);
