0b1e2b4309886e52a8a4ce0fa6925ba1f97e15a5
maariaamm <mariam_97@live.se>
2025-11-14 04:50:36 +0100

    feat: add Class and User models

---
+++ Backend/src/models/class.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+const classSchema = new mongoose.Schema({
+  title: String,
+  description: String,
+  category: String,
+  trainerId: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+  date: String,
+  time: String,
+  maxParticipants: Number,
+  imageUrl: String,
+  participants: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+}, { timestamps: true });
+
+export default mongoose.model("Class", classSchema);
