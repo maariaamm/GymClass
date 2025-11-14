ddb410deb5f4a449356b62209b4ad5459cee6f69
maariaamm <mariam_97@live.se>
2025-11-14 09:35:35 +0100

    fix: update database connection log message for clarity

--- Backend/src/config/db.js
+++ Backend/src/config/db.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI);
-    console.log("MongoDB connected");
+    console.log("Connected to DB:", mongoose.connection.name)
   } catch (error) {
     console.error("MongoDB error:", error);
     process.exit(1);
