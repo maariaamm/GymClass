417f4c0b2cda39fece4464b8bcdd1f5227c9c1cc
maariaamm <mariam_97@live.se>
2026-01-09 20:09:52 +0100

    Add Navbar with navigation links

---
+++ Frontend/src/components/Navbar.jsx
@@ -0,0 +1,104 @@
+import React, { useContext, useState, useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { AuthContext } from "../context/AuthContext";
+import api from "../services/api";
+import "./Navbar.css";
+
+const Navbar = () => {
+  const { user, logout } = useContext(AuthContext);
+  const [search, setSearch] = useState("");
+  const [suggestions, setSuggestions] = useState([]);
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    logout();
+    navigate("/");
+  };
+
+  useEffect(() => {
+    const fetchSuggestions = async () => {
+      if (search.trim().length < 1) {
+        setSuggestions([]);
+        return;
+      }
+
+      try {
+        const res = await api.get(`/classes?search=${search}`);
+        setSuggestions(res.data.slice(0, 5));
+      } catch (err) {
+        console.error("Search failed", err);
+      }
+    };
+
+    fetchSuggestions();
+  }, [search]);
+
+  const handleSelect = (value) => {
+    setSearch(value);
+    setSuggestions([]);
+    navigate(`/classesPage?search=${value}`);
+  };
+
+  const isAdmin = user?.role === "admin";
+
+  return (
+    <nav className="navbar">
+      <div className="navbar-inner">
+        {/* Nav LOGO */}
+        <div className="nav-logo">
+          <Link to="/">GymClass</Link>
+        </div>
+
+        <div className="nav-menu">
+          {user && !isAdmin && <Link to="/">Home</Link>}
+
+          <Link to="/classesPage">Classes</Link>
+
+          {user && !isAdmin && <Link to="/myBookings">My Booked Classes</Link>}
+
+          {user && <Link to="/myProfilePage">My Profile</Link>}
+
+          {/* ADMIN ONLY */}
+          {isAdmin && (
+            <>
+              <Link to="/admin/dashboard">Admin Dashboard</Link>
+            </>
+          )}
+
+          {user ? (
+            <button className="logout-btn" onClick={handleLogout}>
+              Logout
+            </button>
+          ) : (
+            <>
+              <Link to="/login">Login</Link>
+              <Link to="/register">Register</Link>
+            </>
+          )}
+        </div>
+
+        {/* SEARCH */}
+        <div className="nav-search-wrapper">
+          <input
+            type="text"
+            placeholder="Search classes..."
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+          />
+
+          {suggestions.length > 0 && (
+            <ul className="search-dropdown">
+              {suggestions.map((c) => (
+                <li key={c._id} onClick={() => handleSelect(c.title)}>
+                  {c.title}
+                </li>
+              ))}
+            </ul>
+          )}
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
